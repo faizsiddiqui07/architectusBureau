5477408ab8242bc3305f05b8352641c630d90990
Faiz <mohdfaiz.1842@gmail.com>
2023-08-10 20:16:18 +0530

    add

--- script.js
+++ script.js
@@ -10,7 +10,24 @@ var swiper = new Swiper(".mySwiper", {
   });
 
 
-  var swiper = new Swiper(".my-section-slider", {
+  var swiper = new Swiper(".my-project-section-slider", {
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    loop: true,
+    breakpoints: {
+        '576': {
+            slidesPerView: 2,
+            spaceBetween: 8,
+    },
+    '320': {
+        slidesPerView: 1,
+        spaceBetween: 8,
+}
+}
+});
+var swiper = new Swiper(".my-studio-section-slider", {
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
@@ -45,3 +62,46 @@ menubtn2.addEventListener("click",()=>{
     navbarslideleft.classList.remove("active");
     navbarslideicon.classList.remove("active");
 })
+
+// Dropdown Button
+const optionMenu = document.querySelector(".select-menu"),
+       selectBtn = optionMenu.querySelector(".select-btn"),
+       options = optionMenu.querySelectorAll(".option"),
+       sBtn_text = optionMenu.querySelector(".sBtn-text");
+selectBtn.addEventListener("click", () => optionMenu.classList.toggle("active"));       
+options.forEach(option =>{
+    option.addEventListener("click", ()=>{
+        let selectedOption = option.querySelector(".option-text").innerText;
+        sBtn_text.innerText = selectedOption;
+        optionMenu.classList.remove("active");
+    });
+});
+
+const optionMenu2 = document.querySelector(".select-menu2"),
+       selectBtn2 = optionMenu2.querySelector(".select-btn2"),
+       options2 = optionMenu2.querySelectorAll(".option2"),
+       sBtn_text2 = optionMenu2.querySelector(".sBtn-text2");
+selectBtn2.addEventListener("click", () => optionMenu2.classList.toggle("active"));       
+options2.forEach(option2 =>{
+    option2.addEventListener("click", ()=>{
+        let selectedOption = option2.querySelector(".option-text").innerText;
+        sBtn_text2.innerText = selectedOption;
+        optionMenu2.classList.remove("active");
+    });
+});
+
+
+let nav = document.querySelector(".navbar");
+let title = document.querySelector(".title-section"); 
+let searchSection = document.querySelector(".search-section"); 
+window.onscroll = function() {
+    if (document.documentElement.scrollTop > 76) {
+        nav.classList.add("header-scrolled")  
+        title.classList.add("header-fixed")
+        searchSection.classList.add("search-fixed") 
+     } else {
+        nav.classList.remove("header-scrolled")
+        title.classList.remove("header-fixed")
+        searchSection.classList.remove("search-fixed") 
+    }
+}
